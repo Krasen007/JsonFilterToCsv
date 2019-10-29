1e7a369af23fe11c53f65b9ded3398135302a6d8
Krasen Ivanov <krasen007@gmail.com>
2019-10-29 14:00:43 +0200

    Update TargetFramework

--- JsonFilterToCsv/Program.cs
+++ JsonFilterToCsv/Program.cs
@@ -3,18 +3,19 @@
     using JsonFilterToCsv.Model;
     using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.IO;
 
     public static class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("You loaded json file:");
             string pathToJsonFile;
 
             if (args.Length != 0)
             {
                 pathToJsonFile = args[0];
+                Console.WriteLine("You loaded json file:");
                 Console.WriteLine(pathToJsonFile + "\n");
 
                 JsonSerializer serializer = new JsonSerializer();
@@ -31,42 +32,35 @@
             }
 
             // Use for serialization.
-            ////string testJson;
-            ////if (args.Length == 0)
-            ////{
-            ////    Console.WriteLine("No arguments given...");
-            ////    var playerList = new List<Player>();
+            string testJson;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments given.\nGenerating example json file...");
+                var playerList = new List<Player>();
 
-            ////    var testPlayer = new Player
-            ////    {
-            ////        Name = "Krasen Ivanov",
-            ////        PlayerSince = 2019,
-            ////        Position = ".Net Developer",
-            ////        Rating = 67.9
-            ////    };
-            ////    var testPlayer2 = new Player
-            ////    {
-            ////        Name = "Oen Danov",
-            ////        PlayerSince = 1019,
-            ////        Position = "Developer",
-            ////        Rating = 17.9
-            ////    };
+                var testPlayer = new Player
+                {
+                    Name = "Krasen Ivanov",
+                    PlayerSince = 2019,
+                    Position = ".Net Developer",
+                    Rating = 67.9
+                };
+                var testPlayer2 = new Player
+                {
+                    Name = "Test Person",
+                    PlayerSince = 1019,
+                    Position = "Test Developer",
+                    Rating = 17.9
+                };
 
-            ////    playerList.Add(testPlayer);
-            ////    playerList.Add(testPlayer2);
+                playerList.Add(testPlayer);
+                playerList.Add(testPlayer2);
 
-            ////    testJson = JsonConvert.SerializeObject(playerList);
+                testJson = JsonConvert.SerializeObject(playerList);
 
-            ////    using (StreamWriter outputFile = new StreamWriter("results.txt"))
-            ////    {
-            ////        outputFile.WriteLine(testJson);
-            ////    }
-            ////}
-            ////else
-            ////{
-            ////    Player givenPlayer = JsonConvert.DeserializeObject<Player>(args[0]);
-            ////    Console.WriteLine(givenPlayer.Name);
-            ////}
+                using StreamWriter outputFile = new StreamWriter("example.json");
+                outputFile.WriteLine(testJson);
+            }
         }
     }
 }
