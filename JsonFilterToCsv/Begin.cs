1ace6c7ad25df00d47fbf862ddaebff9442da3ca
Krasen <krasen007@gmail.com>
2019-10-30 22:16:35 +0200

    Add checks if less or too many arguments.

--- JsonFilterToCsv/Begin.cs
+++ JsonFilterToCsv/Begin.cs
@@ -1,112 +1,106 @@
+using JsonFilterToCsv.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using JsonFilterToCsv.Model;
-using Newtonsoft.Json;
 
 namespace JsonFilterToCsv
 {
     public class Begin
     {
-        readonly string pathToJsonFile = string.Empty;
-        readonly List<Player> playerList = new List<Player>();
+        private readonly List<Player> playerList = new List<Player>();
+        private readonly List<Player> playerSuperstarList = new List<Player>();
+        private readonly int maxPlayedYears;
+        private readonly double minPlayerRating;
 
-        readonly List<Player> playerSuperstarList = new List<Player>();
-        readonly int maxPlayedYears = 0;
-        readonly double minPlayerRating = 0.0;
-        readonly string savePath = string.Empty;
         public Begin(string[] args)
         {
-            if (args.Length >= 1)
+            if (args.Length >= 1 && args.Length < 5)
             {
-                pathToJsonFile = args[0];
-                LoadJson(pathToJsonFile);
-            }
+                string pathToJsonFile = args[0];
+                if (this.IsJsonLoaded(pathToJsonFile))
+                {
+                    if (args.Length >= 2)
+                    {
+                        this.maxPlayedYears = Convert.ToInt32(args[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please provide the maximum number of years the player has been in NBA:");
+                        this.maxPlayedYears = Convert.ToInt32(Console.ReadLine());
+                    }
 
-            if (args.Length >= 2)
-            {
-                maxPlayedYears = Convert.ToInt32(args[1]);
-                //CalculateMaxYearsPlayed(maxPlayedYears);
-            }
-            else
-            {
-                Console.WriteLine("Please provide maximum number of years the player has been in NBA.");
-            }
+                    if (args.Length >= 3)
+                    {
+                        this.minPlayerRating = Convert.ToInt32(args[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please provide the minimum required rating the player needs to qualify:");
+                        this.minPlayerRating = Convert.ToInt32(Console.ReadLine());
+                    }
 
-            if (args.Length >= 3)
+                    string savePath;
+                    if (args.Length >= 4)
+                    {
+                        this.CalculatePlayerNeededYearAndRating(this.maxPlayedYears, this.minPlayerRating);
+                        savePath = args[3];
+                        this.ExportCSV(savePath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please provide the path for the csv file to be saved.");
+                        savePath = Console.ReadLine();
+                        this.CalculatePlayerNeededYearAndRating(this.maxPlayedYears, this.minPlayerRating);
+                        this.ExportCSV(savePath);
+                    }
+                }
+            } else
             {
-                minPlayerRating = Convert.ToInt32(args[2]);
-                //CalculateMinRequiredRating(minPlayerRating);
-            }
-            else
-            {
-                Console.WriteLine("Please provide minimum required rating the player has to quailify.");
-            }
-
-            if (args.Length >= 4)
-            {
-                CalculatePlayerNeededYearAndRating(maxPlayedYears, minPlayerRating);
-                savePath = args[3];
-                ExportCSV(savePath);
-            }
-            else
-            {
-                Console.WriteLine("Please provide path for the csv file to be saved.");
+                if (args.Length >= 5)
+                {
+                    Console.WriteLine("Too many parameters!");
+                }
             }
         }
 
         private void CalculatePlayerNeededYearAndRating(int maxPlayedYears, double minPlayerRating)
         {
             var currentYear = DateTime.Today.Year;
-            foreach (var player in playerList)
+            foreach (var player in this.playerList)
             {
                 var yearTolerance = player.PlayerSince + maxPlayedYears;
                 if ((currentYear <= yearTolerance) && (player.Rating >= minPlayerRating))
                 {
-                    Console.WriteLine($"Player {player.Name} quialifies with {player.Rating} rating of {minPlayerRating} rating needed.");
-                    Console.WriteLine($"Player {player.Name} qualifies with {player.PlayerSince} years of max {yearTolerance - player.PlayerSince} years needed.");
-                    playerSuperstarList.Add(player);
+                    Console.WriteLine($"Playing since {player.PlayerSince}, {player.Name} meets the required rating of {minPlayerRating} with a score of {player.Rating} achieved in under {maxPlayedYears} years, and qualifies as NBA Superstar in the making.");
+                    this.playerSuperstarList.Add(player);
                 }
             }
         }
 
         private void ExportCSV(string savePath)
         {
-            using StreamWriter outputFile = new StreamWriter(savePath);
-            foreach (var player in playerSuperstarList)
+            if (this.playerSuperstarList.Count == 0)
             {
-                outputFile.WriteLine(player.Name + "," + player.Rating);
+                Console.WriteLine("No SuperStar found among your players.");
             }
-        }
-
-        private void CalculateMinRequiredRating(double minPlayerRating)
-        {
-            foreach (var player in playerList)
+            else
             {
-                if (player.Rating >= minPlayerRating)
+                using StreamWriter outputFile = new StreamWriter(savePath + "\\SuperStars.csv");
+                foreach (var player in this.playerSuperstarList)
                 {
-                    Console.WriteLine($"Player {player.Name} quialifies with {player.Rating} rating of {minPlayerRating} rating needed.");
+                    outputFile.WriteLine(player.Name + "," + String.Format("{0:0.0}", player.Rating));
                 }
+                outputFile.Dispose();
+                Console.WriteLine("\nThese are your SuperStars!\nSuccesfuly saved to " + savePath + "\\SuperStars.csv");
             }
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
         }
 
-        private void CalculateMaxYearsPlayed(int maxPlayedYears)
+        private bool IsJsonLoaded(string pathToJsonFile)
         {
-            var currentYear = DateTime.Today.Year;
-            foreach (var player in playerList)
-            {
-                var yearTolerance = player.PlayerSince + maxPlayedYears;
-                if (currentYear <= yearTolerance)
-                {
-                    //playerSuperstarList.Add(player.Name);
-                    Console.WriteLine($"Player {player.Name} qualifies with {player.PlayerSince} years of max {yearTolerance - player.PlayerSince} years needed.");
-                }
-            }
-        }
-
-        private void LoadJson(string pathToJsonFile)
-        {
-            Console.WriteLine("You loaded json file:");
+            Console.WriteLine("You selected json:");
             Console.WriteLine(pathToJsonFile + "\n");
 
             try
@@ -119,15 +113,18 @@ namespace JsonFilterToCsv
 
                     foreach (var player in jsonFile)
                     {
-                        playerList.Add(player);
+                        this.playerList.Add(player);
                     }
-                    Console.WriteLine("Json Loaded succesfuly...");
                 }
+                Console.WriteLine("Json Loaded succesfuly...\n");
+                return true;
             }
             catch (FileNotFoundException e)
             {
                 Console.WriteLine(e.Message);
-                throw;
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+                return false;
             }
         }
     }
--- JsonFilterToCsv/Program.cs
+++ JsonFilterToCsv/Program.cs
@@ -10,23 +10,36 @@
     {
         private static void Main(string[] args)
         {
-            // Use for serialization.            
+            // This is used when the app is run on a PC using ',' (comma) as a decimal separator.
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+
+            string localVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version!.ToString();
+            Console.Title = "Super Star Finder " + localVer;
+
             if (args.Length == 0)
             {
-                IfNoArgsGenerateExapmleJson();
+                IfNoArgsGenerateSampleJson();
             }
             else
             {
-                new Begin(args);
+                _ = new Begin(args);
             }
         }
 
-        private static void IfNoArgsGenerateExapmleJson()
+        private static void IfNoArgsGenerateSampleJson()
         {
             string testJson;
             List<Player> playerList = new List<Player>();
 
-            Console.WriteLine("No arguments given.\nGenerating example json file...");
+            Console.WriteLine("No arguments provided.\n" +
+                "Please indicate the path to the .json file,\n" +
+                "the maximum number of years the player has played to qualify,\n" +
+                "the player's minimum rating, and\n" +
+                "the path for CSV export, separated with a space.\n" +
+                "Generating sample .json file in the app folder...");
 
             var testPlayer = new Player
             {
@@ -38,9 +51,9 @@
             var testPlayer2 = new Player
             {
                 Name = "Test Person",
-                PlayerSince = 1019,
+                PlayerSince = 2015,
                 Position = "Test Developer",
-                Rating = 17.9
+                Rating = 47.9
             };
 
             playerList.Add(testPlayer);
@@ -48,7 +61,13 @@
 
             testJson = JsonConvert.SerializeObject(playerList);
             using StreamWriter outputFile = new StreamWriter("example.json");
-            outputFile.WriteLine(testJson);
+            {
+                outputFile.WriteLine(testJson);
+                outputFile.Dispose();
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
